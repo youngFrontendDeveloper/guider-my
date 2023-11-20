1c308425ae577c5dbc7f70e95d350549d9c5e446
Nina <frontend-developer@inbox.ru>
2023-11-20 16:45:13 +0300

    Закончили стилизацию

--- src/entities/categories/Categories.tsx
+++ src/entities/categories/Categories.tsx
@@ -1,19 +1,16 @@
-import styles from "./Categories.module.scss";
-import {getCategories} from "@/services/getCategories";
-import {CategoriesItem} from "@/entities/categories/categoriesItem/ui";
-import {CategoriesType} from "@/types/CategoriesType";
-
-
+import styles from './Categories.module.scss';
+import { getCategories } from '@/services/getCategories';
+import { CategoriesItem } from '@/entities/categories/categoriesItem/ui';
+import { CategoriesType } from '@/types/CategoriesType';
 
 export async function Categories() {
-    const categories: CategoriesType[] = await getCategories();
+  const categories: CategoriesType[] = await getCategories();
 
-    return (
-        <section className={styles.categories}>
-            {
-                categories.map((item:CategoriesType ) => <CategoriesItem key={item.id} item={item} />)
-            }
-        </section>
-
-    )
+  return (
+    <section className={styles.categories}>
+      {categories.map((item: CategoriesType) => (
+        <CategoriesItem key={item.id} item={item} />
+      ))}
+    </section>
+  );
 }
